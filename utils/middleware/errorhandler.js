43a9f5d76242d9875322590736c7af3e53bf6c1e
Luis Vazquez <luvazpa@gmail.com>
2021-01-19 13:41:31 -0600

    Error handler fixed

--- utils/middleware/errorhandler.js
+++ utils/middleware/errorhandler.js
@@ -1,16 +1,17 @@
 const boom = require('@hapi/boom');
+const debug = require('debug')('app:server');
 const { config } = require('../../config');
 
 function withErrorStack(error, stack) {
   if (config.dev) {
     return { ...error, stack };
   }
+
   return error;
 }
 
 function logErrors(err, req, res, next) {
-  // eslint-disable-next-line no-console
-  console.log(err);
+  debug(err);
   next(err);
 }
 
@@ -18,21 +19,21 @@ function wrapErrors(err, req, res, next) {
   if (!err.isBoom) {
     next(boom.badImplementation(err));
   }
+
   next(err);
 }
 
-// eslint-disable-next-line no-unused-vars
 function errorHandler(err, req, res, next) {
+  // eslint-disable-line
   const {
     output: { statusCode, payload },
   } = err;
-
   res.status(statusCode);
-  res.json(withErrorStack(err.message, err.stack));
+  res.json(withErrorStack(payload, err.stack));
 }
 
 module.exports = {
   logErrors,
-  errorHandler,
   wrapErrors,
+  errorHandler,
 };
